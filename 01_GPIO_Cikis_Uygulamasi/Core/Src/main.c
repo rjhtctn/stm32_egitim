9188f140ed4958e5d83dd26ec1af221fb72c3cf1
Rojhat Çetin <201903327+rjhtctn@users.noreply.github.com>
2025-12-17 00:26:04 +0300

    ledi 1 saniye aralıklarla yakıp söndürme

--- 01_GPIO_Cikis_Uygulamasi/Core/Src/main.c
+++ 01_GPIO_Cikis_Uygulamasi/Core/Src/main.c
@@ -99,6 +99,9 @@ int main(void)
   {
     /* USER CODE END WHILE */
 	  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
+	  HAL_Delay(1000);
+	  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
+	  HAL_Delay(1000);
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
